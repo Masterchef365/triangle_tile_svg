9e1669e619580b513c9c83d7c8caaf8242562006
Masterchef365 <duncan.freeman1@gmail.com>
2021-09-19 22:34:11 -0700

    Makes triangles

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use anyhow::{bail, Context, Result};
 use std::io::Read;
-use std::path::Path;
+use std::path::{Path, PathBuf};
+use svg::node::element::{path::Data as SvgData, Path as SvgPath};
+use svg::Node;
 
 fn main() -> Result<()> {
     // Arg parsing
@@ -12,7 +14,11 @@ fn main() -> Result<()> {
             program_name
         )
     };
-    let image_path = args.next().with_context(usage)?;
+
+    let image_path: PathBuf = args.next().with_context(usage)?.into();
+    //let svg_path = image_path.parent().expect("Image path has no parent").join(image_path.file_stem().
+    let svg_path = "out.svg";
+
     let n_vertical_tris: usize = args
         .next()
         .unwrap_or("30".to_string())
@@ -28,30 +34,59 @@ fn main() -> Result<()> {
     let (image_width, image_data) = load_png_from_path(image_path).context("Loading image")?;
     let image_height = image_data.len() / (image_width * 3);
 
-    // Base divided by height, so multiplying height times this amount gives you the base
-    let base_per_height: f32 = 1. / (3.0_f32).sqrt();
-    
     // Number of triangles horizontally
-    let n_horiz_tris = image_width * n_vertical_tris / image_height;
+    let n_horiz_tris = (image_width * n_vertical_tris) / image_height;
 
     // Half of the width of the base of a triangle. Useful for stepping along the grid
     let half_triangle_width = triangle_height / (3.0_f32).sqrt();
 
     // Generate triangles
+    let mut document = svg::Document::new().set(
+        "viewBox",
+        (
+            0,
+            0,
+            n_horiz_tris as f32 * half_triangle_width,
+            n_vertical_tris as f32 * triangle_height,
+        ),
+    );
+
     let mut y = 0.0;
     for row in 0..n_vertical_tris {
         let mut x = 0.0;
         for col in 0..n_vertical_tris {
             let points_up = (row & 1 == 0) != (col & 1 == 0);
+            document.append(triangle_at(x, y, half_triangle_width, triangle_height, points_up));
             
             x += half_triangle_width;
         }
         y += triangle_height;
     }
 
-    dbg!(width, height);
+    svg::save(svg_path, &document).context("Saving document")?;
 
     Ok(())
+}
+
+fn triangle_at(x: f32, y: f32, half_width: f32, height: f32, points_up: bool) -> SvgPath {
+    let data = if points_up {
+        SvgData::new()
+            .move_to((x, y))
+            .line_by((-half_width, height))
+            .line_by((half_width * 2., 0.))
+    } else {
+        SvgData::new()
+            .move_to((x, y - height))
+            .line_by((-half_width, -height))
+            .line_by((half_width * 2., 0.))
+    }
+    .close();
+
+    SvgPath::new()
+        .set("fill", "none")
+        .set("stroke", "black")
+        .set("stroke-width", 3)
+        .set("d", data)
 }
 
 fn load_png_from_path<P: AsRef<Path>>(path: P) -> Result<(usize, Vec<u8>)> {
